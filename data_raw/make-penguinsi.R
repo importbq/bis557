d89a6c743e63c9d2c001c3f71ff967e672506618
antoniojustin <54635005+antoniojustin@users.noreply.github.com>
2020-09-09 00:22:09 -0400

    Initial commit

---
+++ data_raw/make-penguinsi.R
@@ -0,0 +1,2 @@
+# create the data for the package
+use_data("name of the data frame")
---
+++ data_raw/penguinisi-doc.R
@@ -0,0 +1,9 @@
+#' Data from the IMAP study
+#' @name penguinsi
+#' @docType data
+#' @rdname penguinsi #this appears in the man directory
+#' @description
+#' @references
+#' @keywords
+
+NULL #no R code
